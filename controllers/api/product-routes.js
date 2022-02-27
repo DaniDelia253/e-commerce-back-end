28e7961cc32248ddef54cdfbdfb58cebe2066458
DaniDelia253 <90367091+DaniDelia253@users.noreply.github.com>
2022-02-26 21:49:49 -0600

    Category and Tag Routes updated

--- controllers/api/product-routes.js
+++ controllers/api/product-routes.js
@@ -57,10 +57,11 @@ router.get('/:id', (req, res) => {
 router.post('/', (req, res) => {
   /* req.body should look like this...
     {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
+      "product_name": "Necklace",
+      "price": 200.00,
+      "stock": 6,
+      "tagIds": [7],
+      "category_id": 6
     }
   */
   Product.create(req.body)
@@ -91,7 +92,7 @@ router.put('/:id', (req, res) => {
   Product.update(req.body, {
     where: {
       id: req.params.id,
-    },
+    }
   })
     .then((product) => {
       // find all associated tags from ProductTag
